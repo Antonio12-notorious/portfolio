de027740b609dbffa05f872ee78a77c2be310467
Vanildo <vanildoantonio19@gmail.com>
2026-04-29 14:42:39 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'sonner';
-import ScrollToTop from './components/ScrollToTop';
-import MainLayout from './layouts/MainLayout';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Services from './pages/Services';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "sonner";
+import { lazy, Suspense } from "react";
+
+import ScrollToTop from "./components/ScrollToTop";
+import MainLayout from "./layouts/MainLayout";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Projects = lazy(() => import("./pages/Projects"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Services = lazy(() => import("./pages/Services"));
 
 export default function App() {
   return (
     <Router>
       <ScrollToTop />
       <Toaster position="bottom-right" richColors />
+
       <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/servicos" element={<Services />} />
-          <Route path="/projectos" element={<Projects />} />
-          <Route path="/contacto" element={<Contact />} />
-        </Routes>
+        <Suspense fallback={<div className="p-6">A carregar...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/servicos" element={<Services />} />
+            <Route path="/projectos" element={<Projects />} />
+            <Route path="/contacto" element={<Contact />} />
+          </Routes>
+        </Suspense>
       </MainLayout>
     </Router>
   );
